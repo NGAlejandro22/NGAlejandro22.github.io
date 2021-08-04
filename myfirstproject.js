fa5e080f20d46943bad0e1976d4121960c4deef8
NGAlejandro22 <73113636+NGAlejandro22@users.noreply.github.com>
2021-08-04 13:48:52 +0100

    Add files via upload

---
+++ myfirstproject.js
@@ -0,0 +1,7 @@
+
+alert ("Welcome! This is my first project so enjoy the content");
+const name = prompt("What is your name?");
+const message = `Hello, ${name} Welcome to my webpage`
+alert (`Hello ${name}`)
+
+console.log(message)
